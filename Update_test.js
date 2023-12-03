101fc0fca0030190bbfe7fbfb106a440b446fbfe
Schubidu93 <123445256+Schubidu93@users.noreply.github.com>
2023-12-03 21:05:15 +0100

    Create Update_test.js

---
+++ Update_test.js
@@ -0,0 +1,38 @@
+// Stelle eine Verbindung mit GitHub her und überprüfe auf Aktualisierungen
+async function checkGitHubForUpdates() {
+    let githubFileURL = "https://raw.githubusercontent.com/DEIN_REPO/DEIN_FILE.js";
+
+    try {
+        let req = new Request(githubFileURL);
+        let currentCode = importModule("DEIN_FILE");
+        let newCode = await req.loadString();
+
+        if (newCode !== currentCode) {
+            // Wenn der Code sich geändert hat, aktualisiere das Widget
+            await updateWidgetWithNewCode(newCode);
+        }
+    } catch (error) {
+        console.error("Fehler beim Überprüfen auf Aktualisierungen:", error);
+    }
+}
+
+// Funktion zum Aktualisieren des Widget-Codes
+async function updateWidgetWithNewCode(newCode) {
+    try {
+        let fm = FileManager.local();
+        let scriptableDir = fm.documentsDirectory();
+        let widgetPath = scriptableDir + "/DEIN_FILE.js";
+
+        // Speichere den neuen Code in der lokalen Datei
+        fm.writeString(widgetPath, newCode);
+
+        // Lade das aktualisierte Skript in Scriptable
+        let updatedScript = importModule(widgetPath);
+        await updatedScript.main(); // Starte das aktualisierte Skript
+    } catch (error) {
+        console.error("Fehler beim Aktualisieren des Widget-Codes:", error);
+    }
+}
+
+// Führe die Überprüfung auf Aktualisierungen aus
+await checkGitHubForUpdates();
