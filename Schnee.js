3050f8d9f4db93aa0bee6302e5c7b5a98fa7a40c
Schubidu93 <123445256+Schubidu93@users.noreply.github.com>
2023-12-04 00:41:18 +0100

    Update Schnee.js

--- Schnee.js
+++ Schnee.js
@@ -16,19 +16,24 @@
         let snowmanEmoji = "⛄️";
 
         let locationText = widget.addText("München"); // Ortsname über der Zahl
-        locationText.font = Font.boldSystemFont(14); // Kleinere Schriftgröße für den Ortsnamen
+        locationText.font = Font.boldSystemFont(12); // Kleinere Schriftgröße für den Ortsnamen
         locationText.leftAlignText(); // Text linksbündig
         locationText.textColor = Color.black(); // Schwarzer Text
 
         let snowHeightText = widget.addText(snowHeight.toString()); // Schneehöhe als Zahl
-        snowHeightText.font = Font.boldSystemFont(40); // Mittlere Schriftgröße für die Schneehöhe
+        snowHeightText.font = Font.boldSystemFont(48); // Mittlere Schriftgröße für die Schneehöhe
         snowHeightText.leftAlignText(); // Text linksbündig
         snowHeightText.textColor = Color.black(); // Schwarzer Text
 
-        let snowInfoText = widget.addText(`Schneehöhe\n${snowflakeEmoji} ${snowmanEmoji}`); // Schneehöhe Info mit Emojis
-        snowInfoText.font = Font.boldSystemFont(10); // Kleine Schriftgröße für die Schneehöhe Info
+        let snowInfoText = widget.addText(`Schneehöhe`); // Schneehöhe Info mit Emojis
+        snowInfoText.font = Font.boldSystemFont(12); // Kleine Schriftgröße für die Schneehöhe Info
         snowInfoText.leftAlignText(); // Text linksbündig
         snowInfoText.textColor = Color.black(); // Schwarzer Text
+
+        let Extratext = widget.addText(`${snowflakeEmoji} ${snowmanEmoji}`); // Schneehöhe Info mit Emojis
+        Extratext.font = Font.boldSystemFont(20); // Kleine Schriftgröße für die Schneehöhe Info
+        Extratext.leftAlignText(); // Text linksbündig
+        Extratext.textColor = Color.black(); // Schwarzer Text
     } else {
         let text = "Es liegt kein Schnee oder die Daten konnten nicht abgerufen werden.";
         let textItem = widget.addText(text);
