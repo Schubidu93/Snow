b43fec9df4c8641295dd1aa728601009d9153780
Schubidu93 <123445256+Schubidu93@users.noreply.github.com>
2023-12-04 00:56:29 +0100

    Update Schnee3.js

--- Schnee3.js
+++ Schnee3.js
@@ -1,61 +1,50 @@
-const url = "https://www.hnd.bayern.de/schnee/inn/muenchen-stadt-10865/tabelle";
+(async () => {
+    const url = "https://www.hnd.bayern.de/schnee/inn/muenchen-stadt-10865/tabelle";
 
-// Funktion zum Herunterladen der Webseite
-async function downloadPage(url) {
-  const request = new Request(url);
-  return await request.loadString();
-}
+    // Funktion zum Herunterladen der Webseite
+    async function downloadPage(url) {
+        const request = new Request(url);
+        return await request.loadString();
+    }
 
-// Funktion zum Extrahieren der Schneehöhe aus dem HTML
-function extractSnowHeight(html) {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(html, "text/html");
+    // Funktion zum Extrahieren der Schneehöhe aus dem HTML
+    function extractSnowHeight(html) {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, "text/html");
 
-  const table = doc.querySelector("table.tblsort tbody");
-  const rows = table.querySelectorAll("tr.row");
+        const table = doc.querySelector("table.tblsort tbody");
+        const rows = table.querySelectorAll("tr.row");
 
-  const snowData = [];
+        const snowData = [];
 
-  // Gehe durch jede Zeile und extrahiere Schneehöhe
-  rows.forEach(row => {
-    const columns = row.querySelectorAll("td");
-    const snowHeight = parseInt(columns[1].textContent.trim());
+        // Gehe durch jede Zeile und extrahiere Schneehöhe
+        rows.forEach(row => {
+            const columns = row.querySelectorAll("td");
+            const snowHeight = parseInt(columns[1].textContent.trim());
 
-    snowData.push(snowHeight);
-  });
+            snowData.push(snowHeight);
+        });
 
-  // Gib die letzten 50 Schneehöhen zurück
-  return snowData.slice(0, 50);
-}
+        // Gib die letzten 50 Schneehöhen zurück
+        return snowData.slice(0, 50);
+    }
 
-// Funktion zum Erstellen des Diagramms als Bild
-async function createChartImage(snowHeights) {
-  const chart = new Chart(500, 300);
-  chart.addBarSeries(snowHeights, { color: Color.blue() });
+    // Funktion zum Erstellen des Widget-Inhalts
+    async function createWidget() {
+        const html = await downloadPage(url);
+        const snowHeights = await extractSnowHeight(html);
 
-  return chart.getImage();
-}
+        const widget = new ListWidget();
+        const chart = new Chart(500, 300);
+        chart.addBarSeries(snowHeights, { color: Color.blue() });
 
-async function createWidget() {
-  const html = await downloadPage(url);
-  const snowHeights = await extractSnowHeight(html);
+        widget.backgroundImage = chart.getImage();
+        widget.url = url; // Verlinkt das Widget auf die Webseite für weitere Details
 
-  const widget = new ListWidget();
-  const chartImage = await createChartImage(snowHeights);
+        return widget;
+    }
 
-  widget.backgroundImage = chartImage;
-  widget.url = url; // Verlinkt das Widget auf die Webseite für weitere Details
-
-  return widget;
-}
-
-async function main() {
-  const widget = await createWidget();
-  if (config.runsInWidget) {
+    const widget = await createWidget();
     Script.setWidget(widget);
-  } else {
-    widget.presentMedium();
-  }
-}
-
-main();
+    Script.complete();
+})();
