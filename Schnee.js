84b98557a33dcde160269ec22a92706bcfb0a501
Schubidu93 <123445256+Schubidu93@users.noreply.github.com>
2023-12-04 01:05:35 +0100

    Update Schnee.js

--- Schnee.js
+++ Schnee.js
@@ -20,28 +20,28 @@
 
         
         let locationText = widget.addText(`${pinEmoji} München`); // Ortsname über der Zahl
-        locationText.font = Font.boldSystemFont(12); // Kleinere Schriftgröße für den Ortsnamen
+        locationText.font = Font.regularSystemFont(12); // Kleinere Schriftgröße für den Ortsnamen
         locationText.leftAlignText(); // Text linksbündig
         locationText.textColor = Color.black(); // Schwarzer Text
 
         let snowHeightText = widget.addText(snowHeight.toString() + "cm"); // Schneehöhe als Zahl
-        snowHeightText.font = Font.boldSystemFont(40); // Mittlere Schriftgröße für die Schneehöhe
+        snowHeightText.font = Font.regularSystemFont(40); // Mittlere Schriftgröße für die Schneehöhe
         snowHeightText.leftAlignText(); // Text linksbündig
         snowHeightText.textColor = Color.black(); // Schwarzer Text
 
         let snowInfoText = widget.addText(`Schneehöhe`); // Schneehöhe Info mit Emojis
-        snowInfoText.font = Font.boldSystemFont(12); // Kleine Schriftgröße für die Schneehöhe Info
+        snowInfoText.font = Font.regularSystemFont(12); // Kleine Schriftgröße für die Schneehöhe Info
         snowInfoText.leftAlignText(); // Text linksbündig
         snowInfoText.textColor = Color.black(); // Schwarzer Text
 
         let Extratext = widget.addText(`${snowflakeEmoji} ${snowmanEmoji}`); // Schneehöhe Info mit Emojis
-        Extratext.font = Font.boldSystemFont(24); // Kleine Schriftgröße für die Schneehöhe Info
+        Extratext.font = Font.regularSystemFont(24); // Kleine Schriftgröße für die Schneehöhe Info
         Extratext.leftAlignText(); // Text linksbündig
         Extratext.textColor = Color.black(); // Schwarzer Text
     } else {
         let text = "Es liegt kein Schnee oder die Daten konnten nicht abgerufen werden.";
         let textItem = widget.addText(text);
-        textItem.font = Font.boldSystemFont(20); // Schriftgröße erhöht
+        textItem.font = Font.regularSystemFont(20); // Schriftgröße erhöht
         textItem.centerAlignText(); // Text zentrieren
         textItem.textColor = Color.black(); // Schwarzer Text
     }
