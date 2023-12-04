bac882cd9a7a0e7629dfd8d95f94c4758d0caf23
Schubidu93 <123445256+Schubidu93@users.noreply.github.com>
2023-12-04 01:09:47 +0100

    Update Schnee.js

--- Schnee.js
+++ Schnee.js
@@ -1,6 +1,6 @@
 (async () => {
     let widget = new ListWidget();
-    widget.backgroundColor = new Color("#E0FFFF"); // Hellblau für den Hintergrund
+    widget.backgroundColor = new Color("#4682B4"); // Hellblau für den Hintergrund
 
     let url = "https://www.hnd.bayern.de/schnee/inn/muenchen-stadt-10865";
     let html = await new Request(url).loadString();
@@ -22,28 +22,28 @@
         let locationText = widget.addText(`${pinEmoji} München`); // Ortsname über der Zahl
         locationText.font = Font.regularSystemFont(12); // Kleinere Schriftgröße für den Ortsnamen
         locationText.leftAlignText(); // Text linksbündig
-        locationText.textColor = Color.black(); // Schwarzer Text
+        locationText.textColor = Color.white(); // Schwarzer Text
 
         let snowHeightText = widget.addText(snowHeight.toString() + "cm"); // Schneehöhe als Zahl
         snowHeightText.font = Font.regularSystemFont(40); // Mittlere Schriftgröße für die Schneehöhe
         snowHeightText.leftAlignText(); // Text linksbündig
-        snowHeightText.textColor = Color.black(); // Schwarzer Text
+        snowHeightText.textColor = Color.white(); // Schwarzer Text
 
         let snowInfoText = widget.addText(`Schneehöhe`); // Schneehöhe Info mit Emojis
         snowInfoText.font = Font.regularSystemFont(12); // Kleine Schriftgröße für die Schneehöhe Info
         snowInfoText.leftAlignText(); // Text linksbündig
-        snowInfoText.textColor = Color.black(); // Schwarzer Text
+        snowInfoText.textColor = Color.white(); // Schwarzer Text
 
         let Extratext = widget.addText(`${snowflakeEmoji} ${snowmanEmoji}`); // Schneehöhe Info mit Emojis
         Extratext.font = Font.regularSystemFont(24); // Kleine Schriftgröße für die Schneehöhe Info
         Extratext.leftAlignText(); // Text linksbündig
-        Extratext.textColor = Color.black(); // Schwarzer Text
+        Extratext.textColor = Color.white(); // Schwarzer Text
     } else {
         let text = "Es liegt kein Schnee oder die Daten konnten nicht abgerufen werden.";
         let textItem = widget.addText(text);
         textItem.font = Font.regularSystemFont(20); // Schriftgröße erhöht
         textItem.centerAlignText(); // Text zentrieren
-        textItem.textColor = Color.black(); // Schwarzer Text
+        textItem.textColor = Color.white(); // Schwarzer Text
     }
 
     Script.setWidget(widget);
