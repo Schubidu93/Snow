b2b237415aa5b09ee4b3f19a7059db42192aadde
Schubidu93 <123445256+Schubidu93@users.noreply.github.com>
2024-05-23 22:20:13 +0200

    Update Wassertemperatur_BW.js

--- Wassertemperatur_BW.js
+++ Wassertemperatur_BW.js
@@ -1,30 +1,39 @@
 (async () => {
-    // Widget-Parameter
-const widgetParameter = args.widgetParameter;
-
-// Webseite laden und HTML parsen
+// URL der Webseite mit der Wassertemperatur
 const url = 'https://www.wassertemperaturen.net/baden-wuerttemberg/stadtsee_bad_waldsee.html';
-const req = new Request(url);
-const res = await req.loadString();
-const regex = /<P CLASS="strandtemp">(\d+)&deg;C<\/P>/;
-const match = res.match(regex);
 
-let wassertemperatur = 'N/A';
-if (match && match.length > 1) {
-    wassertemperatur = match[1] + '°C';
+// Funktion zum Laden und Parsen der Webseite
+async function fetchWaterTemperature() {
+    const req = new Request(url);
+    const html = await req.loadString();
+    const regex = /<P CLASS="strandtemp">(\d+)&deg;C<\/P>/;
+    const match = html.match(regex);
+
+    if (match && match.length > 1) {
+        return match[1] + '°C';
+    } else {
+        return 'N/A';
+    }
 }
 
-// Widget anzeigen
-let widget = new ListWidget();
-widget.addText('Wassertemperatur:');
-widget.addSpacer(4);
-widget.addText(wassertemperatur);
-widget.presentMedium();
+// Hauptfunktion zur Erstellung des Widgets
+async function createWidget() {
+    const wassertemperatur = await fetchWaterTemperature();
+    
+    let widget = new ListWidget();
+    widget.addText('Wassertemperatur:');
+    widget.addSpacer(4);
+    widget.addText(wassertemperatur);
+    
+    return widget;
+}
 
+// Widget erstellen und anzeigen
+let widget = await createWidget();
 if (config.runsInWidget) {
-  Script.setWidget(widget);
+    Script.setWidget(widget);
 } else {
-  widget.presentSmall();
+    widget.presentSmall();
 }
 Script.complete();
 })();
